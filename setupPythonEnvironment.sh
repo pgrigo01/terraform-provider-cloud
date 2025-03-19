951331c48869a1a0b5452479b92bf2feb396f0eb
pgrigo01 <pgrigo01@ucy.ac.cy>
2025-03-19 20:51:43 +0200

    modified setupPythonEnvironment.sh

--- setupPythonEnvironment.sh
+++ setupPythonEnvironment.sh
@@ -22,9 +22,9 @@ pip install --upgrade pip
 pip install flask
 
 # Download and install Google Chrome
-#wget -q -O google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#sudo apt install -y ./google-chrome.deb
-#rm google-chrome.deb
+wget -q -O google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome.deb
+rm google-chrome.deb
 
 # Install necessary Python packages
 pip3 install selenium pandas webdriver-manager
