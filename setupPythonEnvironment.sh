20391dec5c2666dc2ad993c2bfabf7b825836cfd
Panagiotis Grigoriou <pgrigo01@101ws7.in.cs.ucy.ac.cy>
2025-03-19 19:30:32 +0200

    Added a simple script

--- setupPythonEnvironment.sh
+++ setupPythonEnvironment.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Activate the virtual environment
-sudo apt update
-sudo apt install python3-venv
-sudo apt install python3-pip
+#sudo apt update
+#sudo apt install python3-venv
+#sudo apt install python3-pip
 pip install flask
 
 python3 -m venv myenv
 source myenv/bin/activate
-sudo apt install python3-pip
+#sudo apt install python3-pip
 
 
 
@@ -22,9 +22,9 @@ pip install --upgrade pip
 pip install flask
 
 # Download and install Google Chrome
-wget -q -O google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install -y ./google-chrome.deb
-rm google-chrome.deb
+#wget -q -O google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo apt install -y ./google-chrome.deb
+#rm google-chrome.deb
 
 # Install necessary Python packages
 pip3 install selenium pandas webdriver-manager
---
+++ simpleinstall.sh
@@ -0,0 +1,6 @@
+source myenv/bin/activate
+pip install flask
+pip install pandas
+pip install selenium
+python3 firefox.py 
+pip install webdriver_manager
