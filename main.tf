05642f45688a5e9e5a3058ffccc2d04c0c6af056
pgrigo01 <pgrigo01@ucy.ac.cy>
2025-04-22 22:13:46 +0300

    Removed .terraform dir

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     cloudlab = {
       source  = "pgrigo01/cloudlab"
-      version = "1.0.3" 
+      version = "1.0.2" 
     }
   }
 }
@@ -39,12 +39,12 @@ provider "cloudlab" {
 # }
 
 
-# resource "cloudlab_simple_experiment" "experiment4"{
-#     name = "utahexperiment"
-#     routable_ip = true
-#     image        = "UBUNTU 22.04"
-#     aggregate    = "utah.cloudlab.us"
-# }
+resource "cloudlab_simple_experiment" "experiment4"{
+    name = "utahexperiment"
+    routable_ip = true
+    image        = "UBUNTU 22.04"
+    aggregate    = "utah.cloudlab.us"
+}
 
 # resource "cloudlab_simple_experiment" "experiment5"{
 #     name = "utahexperiment2"
@@ -53,9 +53,9 @@ provider "cloudlab" {
 #     aggregate    = "utah.cloudlab.us"
 # }
 
-# resource "cloudlab_simple_experiment" "experiment6"{
-#     name = "utahexperiment3"
-#     routable_ip = true
-#     image        = "UBUNTU 24.04"
-#     aggregate    = "utah.cloudlab.us"
-# }
+resource "cloudlab_simple_experiment" "experiment6"{
+    name = "utahexperiment3"
+    routable_ip = true
+    image        = "UBUNTU 24.04"
+    aggregate    = "utah.cloudlab.us"
+}
